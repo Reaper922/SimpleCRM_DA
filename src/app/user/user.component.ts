322f8a4ab92e9f2451bd9f28d24e2273a1f210bf
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-19 22:10:04 +0200

    Adder user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddUserComponent);
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    // });
+  }
+}
