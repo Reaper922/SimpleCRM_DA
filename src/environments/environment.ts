62e1755b59c577be68e63a6876cdbcd0f3d8e3ff
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-20 23:42:39 +0200

    Added environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'simple-crm-a7522',
+    appId: '1:985382676121:web:f453f65a7e60a17bedfa08',
+    storageBucket: 'simple-crm-a7522.appspot.com',
+    apiKey: 'AIzaSyB6majMg0FLhLbbK2i9O5RBhxPTVXWNyEg',
+    authDomain: 'simple-crm-a7522.firebaseapp.com',
+    messagingSenderId: '985382676121',
+  },};
