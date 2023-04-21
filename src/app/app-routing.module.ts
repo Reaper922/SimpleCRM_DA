f7ffa7829bdba12b6c6e05db517b37682cf078c4
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-21 21:44:58 +0200

    Added dashboard and user routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'user/:id', component: UserDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
