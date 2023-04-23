5141c71a4b629f63cb139985725b50a6a4ebcf37
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-23 17:05:44 +0200

    Adjusted user model

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -1,10 +1,22 @@
-export class User {
+export type UserData = {
+    id?: string;
+    firstname: string;
+    lastname: string;
+    adress: string;
+    postalCode: string;
+    city: string;
+    birthdate: number;
+    email: string
+}
+
+export class User implements UserData {
     firstname!: string;
     lastname!: string;
     adress!: string;
     postalCode!: string;
     city!: string;
     birthdate!: number;
+    email!: string;
 
     constructor(userObj?: any) {
         this.firstname = userObj ? userObj.firstname : '';
@@ -13,16 +25,18 @@ export class User {
         this.postalCode = userObj ? userObj.postalCode : '';
         this.city = userObj ? userObj.city : '';
         this.birthdate = userObj ? userObj.birthdate : '';
+        this.email = userObj ? userObj.email : '';
     }
 
-    toJson() {
+    toJson(): UserData {
         return {
             'firstname': this.firstname,
             'lastname': this.lastname,
             'adress': this.adress,
             'postalCode': this.postalCode,
             'city': this.city,
-            'birthdate': this.birthdate
+            'birthdate': this.birthdate,
+            'email': this.email
         }
     }
 }
